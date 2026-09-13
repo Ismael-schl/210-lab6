c9bc8075b409dbe15e4e0ab79ca839cc7bec32fb
Ismael-schl <ihadi678@insite.4cd.edu>
2026-09-12 18:49:20 -0700

    10min update

--- main.cpp
+++ main.cpp
@@ -2,4 +2,28 @@
 #include <iostream>
 using namespace std;
 
+const int arraySize =5;
 
+void enterArrayData(double* array, int size);
+void outputArrayData(double* array, int size);
+double sumArray(double* array, int size);
+
+int main() {
+    double* arr = new double[arraySize];
+
+    enterArrayData(arr, arraySize);
+    outputArrayData(arr, arraySize);
+    sumArray(arr, arraySize);
+    return 0;
+}
+
+void enterArrayData(double* array, int size) {
+
+}
+void outputArrayData(double* array, int size) {
+
+}
+
+double sumArray(double* array, int size) {
+
+}
