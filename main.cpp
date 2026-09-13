4c5d2c13dde30f9c525a5c5d25be49ffea4a5bd2
Ismael-schl <ihadi678@insite.4cd.edu>
2026-09-12 19:05:29 -0700

    10min update

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-const int arraySize =5;
+const int arraySize = 5;
 
 void enterArrayData(double* array, int size);
 void outputArrayData(double* array, int size);
@@ -13,17 +13,31 @@ int main() {
 
     enterArrayData(arr, arraySize);
     outputArrayData(arr, arraySize);
-    sumArray(arr, arraySize);
+    cout << "Sum of values: " << sumArray(arr, arraySize);
     return 0;
 }
 
 void enterArrayData(double* array, int size) {
-
+    cout << "Data entry for the array: " << endl;
+    for (int i = 0; i < size; i++){
+        cout << "    > Element #" << i << ": ";
+        cin >> *(array + i);
+        cout << endl;
+    }
+    cout << "Data entry complete." << endl;
 }
 void outputArrayData(double* array, int size) {
-
+    cout << "Outputting array elements: ";
+    for (int i = 0; i < size; i++){
+        cout << *(array + i) << " ";
+    }
+    cout << endl;
 }
 
 double sumArray(double* array, int size) {
-
+    double sum = 0.0;
+    for (int i = 0; i < size; i++){
+        sum += *(array + i);
+    }
+    return sum;
 }
